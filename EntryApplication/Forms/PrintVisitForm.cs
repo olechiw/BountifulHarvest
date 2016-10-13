f2ecb21bf13d7e60f420117c022b318be02e3f2e
pianist1119 <pianist1119@gmail.com>
2016-10-12 21:30:24 -0400

    Officially determined this the alpha release, ready for testing, all new changes will be in a seperate branch.

--- EntryApplication/Forms/PrintVisitForm.cs
+++ EntryApplication/Forms/PrintVisitForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//
+// Bountiful Harvest Alpha Version 1.0 - Jakob Olechiw, 10/12/2016
+//
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
