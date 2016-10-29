1e3ca923d80205e8faa4c97afd443259cf584de6
pianist1119 <pianist1119@gmail.com>
2016-10-29 13:28:22 -0400

    A bit more formatting as I finish up today

--- Common/Handlers/SqlHandler.cs
+++ Common/Handlers/SqlHandler.cs
@@ -10,8 +10,32 @@ using System.Threading.Tasks;
 
 namespace Common
 {
+    // Somewhere to put all of the SqlConstants neatly
+    class Constants
+    {
+        #region Constants for Querying SQL Columns
+
+        // Hardcoded strings for all of the column names in SQL
+        public const string FirstName = "FirstName";
+        public const string LastName = "LastName";
+        public const string MiddleInitial = "MiddleInitial";
+        public const string Gender = "Gender";
+        public const string Family = "Family";
+        public const string DateOfLastVisit = "LastVisit";
+        public const string DateOfBirth = "DateOfBirth";
+        public const string Address = "Address";
+        public const string PhoneNumber = "PhoneNumber";
+        public const string Comments = "Comments";
+        public const string InitialVisitDate = "InitialVisitDate";
+
+        #endregion
+    }
+}
+
+
+namespace Common
+{
     class SqlHandler
     {
-        
     }
 }
